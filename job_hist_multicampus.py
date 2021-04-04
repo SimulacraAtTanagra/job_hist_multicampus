cc5df53308fd793e5c3af869460de5ab48fec011
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-04-04 14:46:24 -0400

    updated job_hist_multicampus.py

--- job_hist_multicampus.py
+++ job_hist_multicampus.py
@@ -5,41 +5,39 @@ Created on Wed Mar 11 14:22:02 2020
 @author: sayers
 """
 
-from src.emailautosend import mailthis
-from src.emailautosend import getemail
+from emailautosend import mailthat
 import os
-from src.cleansheet import *
+from cleansheet import cleansheet
 import pandas as pd
-import re
-from datetime import datetime 
-from matplotlib import pyplot as plt
+from admin import newest, rehead, colclean
 
-def newest(path,fname):
-    files = os.listdir(path)
-    paths = [os.path.join(path, basename) for basename in files if basename.startswith(fname)]
-    return max(paths, key=os.path.getmtime)
 
 path = "S:\\Downloads\\"     # Give the location of the files
 fname = "JOB_HIST"         # Give filename prefix
-df = pd.read_excel(newest(path,fname))  #getting the newest of these files in the directory and converting to df
+#getting the newest of these files in the directory and converting to df
 #stripping out the 2 metadata columns in CJR files
-if re.match("R1013",df.columns.values[0]).group() == "R1013":
-    new_header = df.iloc[1] #grab the first row for the header
-    df = df[2:] #take the data less the header row
-    df.columns = new_header #set the header row as the df header
 #standardizing the column names
-df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
-
-
-"""
-for i in sorted(list(df.columns.values)):
-    print(i)
+def multicampus_file(infolder,fname,outfolder):
+    df=colclean(rehead(pd.read_excel(newest(infolder,fname)),2))
+    multicampus = df[df.id.isin(df[~(df.unit=="YRK01")][df.pay_status.isin(['A','W','P','L'])].id.unique())]
+    outfile=os.path.join(outfolder,"multicampus.xls")
+    multicampus.to_excel(outfile)
+    return(multicampus,outfile)
     
-column_list = df.columns.values.tolist()
-for column_name in column_list:
-      print(df[column_name].unique())
-"""
-multicampus = df[df.id.isin(df[~(df.unit=="YRK01")][df.pay_status.isin(['A','W','P','L'])].id.unique())]
+def communications(fname,distlist):
+    subj='Multicampus report'
+    body= f'\n<html>\n<head>\n<p>Good Day, </p>\n<p> </p>\n<p>Attached find the multicampus employee file.</p>\n<p> </p>\n<p>Best Regards,</p>\n<p>Shane Ayers</p>\n<p>Human Resources Information Systems Manager</p>\n<p>Office of Human Resources</p>\n<p>York College</p>\n<p>The City University of New York</p>\n</body></html>\n'
+    mailthat(subj,to=distlist,html=body,atch=fname)
+    
+def main(infolder,outfolder):
+    fname="JOB_HIST"
+    file=multicampus_file(infolder,fname,outfolder)
+    cleansheet(file)
+    dists='some set; of peoples; emails'
+    #TODO replace this with a read_json call to an existing file
+    communications(file,dists)
 
-multicampus.to_excel("S:\\Downloads\\multicampus.xls")
-df.empl_class.unique()
+
+if "__name__"=="__main__":
+    main("s:\\downloads","y:\\reports")
+    
